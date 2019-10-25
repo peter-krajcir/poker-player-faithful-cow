f0e5387b8078f58aec6a910a2d4e907e7daa6f03
Krajcir, Peter (pk2676) <pk2676@intl.att.com>
2019-10-25 13:32:17 +0200

    improved logic

--- Player.js
+++ Player.js
@@ -1,10 +1,39 @@
 class Player {
   static get VERSION() {
-    return "0.2";
+    return "0.3";
+  }
+
+  static highCard(hole_cards) {
+    let high = hole_cards.filter(card =>
+      ["J", "Q", "K", "A"].includes(card.rank)
+    );
+    return high.length > 0;
   }
 
   static betRequest(gameState, bet) {
-    bet(10000);
+    if (true) {
+      // new logic
+      if (gameState.players[gameState.in_action].hole_cards.length == 2) {
+        if (
+          gameState.players[gameState.in_action].hole_cards[0].suit ===
+            gameState.players[gameState.in_action].hole_cards[1].suit &&
+          highCard(gameState.players[gameState.in_action].hole_cards)
+        ) {
+          // same suit & high card (J, Q, K, A)
+          // go all in
+          bet(10000);
+        } else {
+          bet(
+            gameState.current_buy_in -
+              gameState.players[gameState.in_action].bet +
+              gameState.minimum_raise
+          );
+        }
+      }
+    } else {
+      // previous logic
+      bet(10000);
+    }
   }
 
   static showdown(gameState) {}
